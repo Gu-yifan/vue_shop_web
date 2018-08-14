61b13f03f9e768127563b7b70a579e192ee46484
guyifan <1026545319@qq.com>
2018-08-14 23:20:14 +0800

    主页完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 导入 element-ui 组件库
+import ElementUI from 'element-ui'
+import axios from 'axios'
 
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入图标的样式表
+import './assets/fonts/iconfont.css'
+// 导入 全局的自定义样式表
+import './assets/css/global.css'
+
+// 把 组件库安装到 Vue
+Vue.use(ElementUI)
+
+// 配置 axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 全局配置 axios 的 request 拦截器
+axios.interceptors.request.use(config => {
+  // 通过拦截request请求,主动为 请求头,追加新属性 Authorization,等于 token 值
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+// 导入后台主页组件
+import Home from '@/components/home/Home'
+// 导入 Welcome 组件
+import Welcome from '@/components/home/Welcome'
 
 Vue.use(Router)
 
-export default new Router({
+// 创建路由对象
+const router = new Router({
   routes: [
+    { path: '/', redirect: '/home' },
+    { path: '/login', component: Login },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [{ path: '/welcome', component: Welcome }]
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // 如果访问的 login 页面，直接放行
+  if (to.path === '/login') return next()
+  // 获取 token 字符串
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 如果没有 token 字符串，强制跳转到 登录页
+  if (!tokenStr) return next('/login')
+  // 证明有 token 字符串，直接放行
+  next()
+})
+
+// 把路由对象暴露出去
+export default router
